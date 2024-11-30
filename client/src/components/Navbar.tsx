c097ba8d7555c826aca6b16a10c72487040ac578
l-merta <lukas.merta26@gmail.com>
2024-11-30 01:11:30 +0100

    Navbar

--- client/src/components/Navbar.tsx
+++ client/src/components/Navbar.tsx
@@ -1,9 +1,21 @@
 //import React from 'react'
+import { Link } from "react-router-dom";
 
 const Navbar = () => {
   return (
     <nav className="nav-main">
-      Navbar
+      <div className="logo">
+        <img src="/images/logo.png" alt="Logo icon" />
+        <span className="name">Loopy</span>
+      </div>
+      <div className="line"></div>
+      <div className="links">
+        <Link to="/" className="link link-profile">Profil</Link>
+        <Link to="/" className="link">
+          <i className="fa-solid fa-house"></i>
+          <span>Domů</span>
+        </Link>
+      </div>
     </nav>
   )
 }
